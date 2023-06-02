7824e3581da5b582bca1cc024af532149c6b3b29
asoliev <91322310+asoliev@users.noreply.github.com>
2023-06-02 23:11:13 +0500

    task1 improvement

--- ProfilingTask1/ProfilingTask1/Program.cs
+++ ProfilingTask1/ProfilingTask1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
 using System.Security.Cryptography;
+using System.Text;
 
 string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
 {
@@ -13,23 +14,24 @@ string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
     var passwordHash = Convert.ToBase64String(hashBytes);
     return passwordHash;
 }
-//string NewGeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
-//{
-//    var iterate = 10000;
-//    byte[] hash;
-//    using (var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate))
-//    {
-//        hash = pbkdf2.GetBytes(20);
-//    }
-//    byte[] hashBytes = new byte[36];
-//    Array.Copy(salt, 0, hashBytes, 0, 16);
-//    Array.Copy(hash, 0, hashBytes, 16, 20);
-//    var passwordHash = Convert.ToBase64String(hashBytes);
-//    return passwordHash;
-//}
+string NewGeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+{
+    var iterate = 10000;
+    byte[] hash;
+    using (var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate))
+    {
+        hash = pbkdf2.GetBytes(20);
+    }
+    var hashBuilder = new StringBuilder();
+
+    hashBuilder.Append(Convert.ToBase64String(salt));
+    hashBuilder.Append(Convert.ToBase64String(hash));
+
+    return hashBuilder.ToString();
+}
 
 string _pwdTxt = "Hello";
-var _salt = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6 };
+var _salt = new byte[16];
 Stopwatch stopwatch = new Stopwatch();
 
 stopwatch.Start();
@@ -38,10 +40,11 @@ stopwatch.Stop();
 Console.WriteLine("Old generator work time: {0}ms", stopwatch.ElapsedMilliseconds);
 Console.WriteLine(pwdHash);
 Console.WriteLine();
-Console.ReadKey();
 
-//stopwatch.Restart();
-//pwdHash = NewGeneratePasswordHashUsingSalt(_pwdTxt, _salt);
-//stopwatch.Stop();
-//Console.WriteLine("New generator work time: {0}ms", stopwatch.ElapsedMilliseconds);
-//Console.WriteLine(pwdHash);
+stopwatch.Restart();
+pwdHash = NewGeneratePasswordHashUsingSalt(_pwdTxt, _salt);
+stopwatch.Stop();
+Console.WriteLine("New generator work time: {0}ms", stopwatch.ElapsedMilliseconds);
+Console.WriteLine(pwdHash);
+
+Console.ReadKey();
